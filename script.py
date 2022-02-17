5fdb1d912683091166392eda28b5c149a8255d72
Adam Easterling <adam@middle.app>
2022-02-17 11:32:53 -0800

    Removing some old usused code from a prior version of this script...

--- script.py
+++ script.py
@@ -26,15 +26,6 @@ import time
 import signal
 
 
-COLOR_END = "\x1b[39m"
-F_Red = "\x1b[31m"
-F_Green = "\x1b[32m"
-F_Yellow = "\x1b[33m"
-F_Blue = "\x1b[34m"
-F_Magenta = "\x1b[35m"
-F_Cyan = "\x1b[36m"
-
-
 class timeout:
     def __init__(self, seconds=1, error_message='Timeout'):
         self.seconds = seconds
@@ -58,7 +49,6 @@ def domain_has_ip(domain_name):
                 if "NXDOMAIN" in line:
                     return False
     except Exception as e:
-        # print(f"domain {domain_name} failed to get ip, {e}")
         return True
     return True
 
@@ -69,26 +59,17 @@ def domain_registry(domain_name):
         with timeout(seconds=1):
             entry = whois.whois(domain_name)
     except Exception as e:
-        # print(f"domain {domain_name} lookup failed due to {e}")
         return False
     else:
-        # print(f"domain_registered({domain_name}) ", entry.domain_name)
         return entry.domain_name
 
 
 def domain_taken(domain_name):
-    # print('i')
     if domain_has_ip(domain_name):
         return True
-    # print('d')
     if domain_registry(domain_name):
         return True
     return False
-
-
-_scrabble_file = open('scrabble-words.txt')
-_scrabble_words = _scrabble_file.read()
-_scrabble_words_set = set(word.lower() for word in _scrabble_words.split('\n'))
 
 
 def word_is_english(name):
@@ -109,9 +90,6 @@ if __name__ == "__main__":
             if any(map(name.endswith, ['ed', 's', 'ing', 'ly', 'ize', 'able', 'ish', 'er'])):
                 continue
 
-            if not word_is_english(name):
-                continue
-
             if 5 <= len(name) <= 7:
                 count += 1
 
@@ -122,24 +100,6 @@ if __name__ == "__main__":
                     else:
                         print(f"\n{complete_name}\tAVAILABLE!\n", end='', flush=True)
 
-                # domain_tech = name + '.tech'
-                # domain_nametech = name + 'tech.com'
-                # domain_app = name + '.app'
-                # domain_nameapp = name + 'app.com'
-                # # print(f'trying {domain}')
-
-                # if domain_taken(domain_tech):
-                #     print('.', end='', flush=True)
-                # elif domain_taken(domain_nametech):
-                #     print(F_Red + '.' + COLOR_END, end='', flush=True)
-                # elif domain_taken(domain_app):
-                #     print(F_Green + '.' + COLOR_END, end='', flush=True)
-                # elif domain_taken(domain_nameapp):
-                #     print(F_Yellow + '.' + COLOR_END, end='', flush=True)
-                # else:
-                #     print(f"\n{name}\tAVAILABLE!\n", end='', flush=True)
-
     finally:
         print('count = ', count)
-        _scrabble_file.close()
 
